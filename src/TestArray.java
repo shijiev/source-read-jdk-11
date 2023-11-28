270e7fe9826c6640d27a28b2056f9ce2ccc14a68
shijiev <2424782003@qq.com>
2023-11-28 23:50:52 +0800

    jdk11源码阅读-集合

--- src/TestArray.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class TestArray {
-    public static void main(String[] args) {
-        ArrayList arrayList = new ArrayList();
-        arrayList.add("123");
-    }
-}
