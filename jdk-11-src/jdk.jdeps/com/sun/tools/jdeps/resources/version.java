fc7863c212c29ae15ce7dc0f699f59070c273543
shijiev <2424782003@qq.com>
2023-11-28 22:24:45 +0800

    添加JDK11源码

---
+++ jdk-11-src/jdk.jdeps/com/sun/tools/jdeps/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.jdeps.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "11.0.19+9-LTS-224" },
+            { "jdk", "11.0.19" },
+            { "release", "11.0.19" },
+        };
+    }
+}
---
+++ jdk-11-src/jdk.jshell/jdk/internal/jshell/tool/resources/version.java
@@ -0,0 +1,11 @@
+package jdk.internal.jshell.tool.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "11.0.19+9-LTS-224" },
+            { "jdk", "11.0.19" },
+            { "release", "11.0.19" },
+        };
+    }
+}
